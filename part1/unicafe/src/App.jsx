eaa2e7592b5764b92120c6106c004f510d0cdcee
Pierry A. Pereira <pierryangelo@gmail.com>
2025-01-09 13:30:42 -0300

    app refactored

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,12 +16,39 @@ const Button = ({handleClick, text}) => {
   )
 }
 
-const Statistics = ({good, neutral, bad}) => {
+const StatisticLine = ({text, value}) => {
   return (
     <>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad} </p>
+      <tr>
+        <td>{text}</td><td>{value}</td>
+      </tr>
+    </>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const averageScore = ((good * 1) + (neutral * 0) + (bad * -1)) / total
+  const positiveFeedback = (good / total) * 100 + " %"
+
+  if (total === 0)
+    return (
+      <><p>No feedback given.</p></>
+    )
+
+  return (
+    <>
+      <table>
+        <thead></thead>
+        <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={averageScore} />
+        <StatisticLine text="positive" value={positiveFeedback} />
+        </tbody>
+      </table>
     </>
   )
 }
@@ -49,6 +76,7 @@ function App() {
       <Button handleClick={handleGoodClick} text="good" />
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
+      <Title title="statistics" />
       <Statistics good={goodCount} neutral={neutralCount} bad={badCount} />
     </>
   )
